9a5bec5dadfb2d04451d2346e00f30c34c3aec1d
Cristóbal Domínguez <cristobaldominguez95@gmail.com>
2018-02-28 17:12:52 +0100

    Remove unused property

--- src/app/components/reactive/reactive.component.ts
+++ src/app/components/reactive/reactive.component.ts
@@ -12,7 +12,6 @@ export class ReactiveComponent {
     name: 'Cristóbal',
     lastName: 'Domínguez',
     email: 'cristobaldominguez95@gmail.com',
-    hobbies: ['running', 'coding', 'reading']
   };
   formGroup: FormGroup;
   
