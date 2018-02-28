3fa54d55dbc76f24a47500f2885333420d017cfb
Cristóbal Domínguez <cristobaldominguez95@gmail.com>
2018-02-28 14:01:34 +0100

    Add select, radios and checkboxes dynamically from component data

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -10,13 +10,19 @@ export class TemplateComponent {
 
   user: Object = {
     name: null,
-    lastName: null
+    lastName: null,
+    country: '',
+    gender: null,
+    accept: false
   };
+  countries: string[] = ['Spain', 'Portugal', 'France', 'Germany', 'United Kingdom', 'Italy'];
+  genders: string[] = ['Male', 'Female'];
 
   constructor() { }
 
   save(templateForm: NgForm): void {
     console.log('form submitted');
+    console.log(this.user);
   }
 
 }
